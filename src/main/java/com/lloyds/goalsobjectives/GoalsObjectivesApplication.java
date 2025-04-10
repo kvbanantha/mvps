48af6072fd23b62576239500f8b4f543e040805a
anantha.kommaraju <anantha.kommaraju@lloydsbanking.dev>
2025-04-10 04:54:26 +0000

    Initial Commit

---
+++ src/main/java/com/lloyds/goalsobjectives/GoalsObjectivesApplication.java
@@ -0,0 +1,13 @@
+package com.lloyds.goalsobjectives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalsObjectivesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoalsObjectivesApplication.class, args);
+  }
+
+}
